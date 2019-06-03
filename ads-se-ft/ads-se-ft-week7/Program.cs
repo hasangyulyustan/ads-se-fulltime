bca0eaec45e57e1fc61a4f7deb51f49988c3f2a6
Hasan Gyulyustan <hasan_17@abv.bg>
2019-06-03 15:08:59 +0100

    Week 7 exercises added.

---
+++ ads-se-ft/ads-se-ft-week7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ads_se_ft_week7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
