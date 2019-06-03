af7e5f3ff0b32769c07fd6beecc3ad9cdbe56fa1
Hasan Gyulyustan <hasan_17@abv.bg>
2019-06-03 14:55:39 +0100

    Week 6 added.

---
+++ ads-se-ft/ads-se-ft-week6/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ads_se_ft_week6
+{
+    class Program
+    {
+        // Merge sort
+        static public void MainMerge(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[25];
+            int i, eol, num, pos;
+            eol = (mid - 1);
+            pos = left;
+            num = (right - left + 1);
+
+            while ((left <= eol) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[pos++] = numbers[left++];
+                else
+                    temp[pos++] = numbers[mid++];
+            }
+            while (left <= eol)
+                temp[pos++] = numbers[left++];
+            while (mid <= right)
+                temp[pos++] = numbers[mid++];
+            for (i = 0; i < num; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
+
+        static public void MergeSort(int[] numbers, int left, int right)
+        {
+            int mid;
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                MergeSort(numbers, left, mid);
+                MergeSort(numbers, (mid + 1), right);
+                MainMerge(numbers, left, (mid + 1), right);
+            }
+        }
+
+        // Insertion Sort
+        static void InsertionSort(int[] inputarray)
+        {
+            for (int i = 0; i < inputarray.Length - 1; i++)
+            {
+                int j = i + 1;
+
+                while (j > 0)
+                {
+                    if (inputarray[j - 1] > inputarray[j])
+                    {
+                        int temp = inputarray[j - 1];
+                        inputarray[j - 1] = inputarray[j];
+                        inputarray[j] = temp;
+
+                    }
+                    j--;
+                }
+            }
+        }
+
+        // Quick sort
+        static void QuickSort(int[] arr, int start, int end)
+        {
+            int i;
+            if (start < end)
+            {
+                i = Partition(arr, start, end);
+
+                QuickSort(arr, start, i - 1);
+                QuickSort(arr, i + 1, end);
+            }
+        }
+
+        static int Partition(int[] arr, int start, int end)
+        {
+            int temp;
+            int p = arr[end];
+            int i = start - 1;
+
+            for (int j = start; j <= end - 1; j++)
+            {
+                if (arr[j] <= p)
+                {
+                    i++;
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            temp = arr[i + 1];
+            arr[i + 1] = arr[end];
+            arr[end] = temp;
+            return i + 1;
+        }
+
+        // Shell sort
+        private void ShellSort(int[] array)
+        {
+            int n = array.Length;
+            int gap = n / 2;
+            int temp;
+
+            while (gap > 0)
+            {
+                for (int i = 0; i + gap < n; i++)
+                {
+                    int j = i + gap;
+                    temp = array[j];
+
+                    while (j - gap >= 0 && temp < array[j - gap])
+                    {
+                        array[j] = array[j - gap];
+                        j = j - gap;
+                    }
+
+                    array[j] = temp;
+                }
+
+                gap = gap / 2;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
